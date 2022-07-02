e6c37c542a5d193dfa0cbe35473c4a2d0cf51897
yusooo <yusaei1107@gmail.com>
2022-07-02 23:58:33 +0900

    Express Server Using

--- 004_ExpressServer/04_ExpressNoneDynamic/express_none.js
+++ 004_ExpressServer/04_ExpressNoneDynamic/express_none.js
@@ -1,1 +1,8 @@
 app.use(express.static("public")); // public 폴더에 있는 모든 정적 파일을 URL로 제공할 수 있게 됨
+
+// => public 폴더의 파일을 브라우저 통해 로드 가능
+/*
+http://localhost:3000/css/style.css
+http://localhost:3000/images/logo.jpg
+http://localhost:3000/js/common.js
+*/
