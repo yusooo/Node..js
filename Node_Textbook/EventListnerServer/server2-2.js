3a87b026e567c1cd03775284302449c62297051b
yusooo <yusaei1107@gmail.com>
2022-07-19 23:10:12 +0900

    Node Server

--- Node_Textbook/EventListnerServer/server2-2.js
+++ Node_Textbook/EventListnerServer/server2-2.js
@@ -4,7 +4,7 @@ const http = requird('fs').prommise;
 http.createServer(async (req, res)=>{
     try{
         const data = await fs.readFile('./server2-1.html');
-        res.writehead(200, 'Content-Type': 'text/html; charset=utf-8')
+        res.writehead(200, 'Content-Type', 'text/html; charset=utf-8')
     }
     catch(err){
         console.error(err);
