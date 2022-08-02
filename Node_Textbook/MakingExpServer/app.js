64d1d1c4f5da7f0dccf75879ee34713f0d7871e9
yusooo <yusaei1107@gmail.com>
2022-08-02 23:55:22 +0900

    mid

--- Node_Textbook/MakingExpServer/app.js
+++ Node_Textbook/MakingExpServer/app.js
@@ -1,9 +1,31 @@
 const express = require('express'); // 웹 서버 모듈 (http 모듈 포함)
 const path = require('path') // 파일 경로 지정 모듈
+const morgan = require('morgan'); // 요청 및 응답 관련 정보를 콘솔에 기록
+// static : 정적 파일 제공 라우터 역할, 따로 설치 X (express 객체 안에서 꺼내 사용 : app.use('request path', express.static(dir path))과 같음)
+const cookieParsesr = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express(); // express 모듈을 app 변수에 할당 : express 내부에 http 모듈 내장 => 서버 역할 가능
 app.set('port', process.env.PORT || 3000); // app.set('port', 포트) => 서버 실행 포트 설정 : process.env 객체에 PORT 속성이 존재할 시 그 값 사용 / default 3000
 // 추후 데이터를 app.get(key)로 가져올 수 있음
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParsesr(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
 
 app.use((req, res, next)=>{
     console.log('모든 요청에 다 실행됩니다.');
